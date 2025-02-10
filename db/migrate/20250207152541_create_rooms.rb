8051a9df4d76fd98bd1a270f63f1a40fc7040517
dwnwp <dewsinglor@gmail.com>
2025-02-11 00:05:04 +0700

    add default room

--- db/migrate/20250207152541_create_rooms.rb
+++ db/migrate/20250207152541_create_rooms.rb
@@ -1,11 +1,15 @@
 class CreateRooms < ActiveRecord::Migration[8.0]
-  def change
+  def up
     create_table :rooms do |t|
       t.string :name
-
       t.timestamps
     end
+
     Room.create!(name: 'Room 1')
     Room.create!(name: 'Room 2')
+  end
+
+  def down
+    drop_table :rooms
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,24 +21,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_10_051905) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
   create_table "messages", force: :cascade do |t|
     t.string "content"
     t.integer "room_id", null: false
@@ -72,8 +54,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_10_051905) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
   add_foreign_key "sessions", "users"
