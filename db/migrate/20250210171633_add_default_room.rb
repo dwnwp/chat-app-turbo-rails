48d096e3e4f18db2dbd79a9750cec2ab3eaf3bb9
dwnwp <dewsinglor@gmail.com>
2025-02-11 00:24:06 +0700

    add default room

--- db/migrate/20250210171633_add_default_room.rb
+++ db/migrate/20250210171633_add_default_room.rb
@@ -1,6 +1,10 @@
 class AddDefaultRoom < ActiveRecord::Migration[8.0]
-  def change
+  def up
     Room.create!(name: 'Room 1')
     Room.create!(name: 'Room 2')
+  end
+
+  def down
+    Room.where(name: ['Room 1', 'Room 2']).destroy_all
   end
 end
